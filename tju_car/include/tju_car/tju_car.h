009f98d10b73ce633c2be04465f14f093eb6f631
tevenfeng <fengdingwen@outlook.com>
2018-08-13 19:41:08 +0800

    add emergency brake button

--- tju_car/include/tju_car/tju_car.h
+++ tju_car/include/tju_car/tju_car.h
@@ -10,21 +10,25 @@ public:
     TjuCar();
 
 private:
-
+	// file descriptor for opening connection to ttyTHS2
 	int fd;
-
+	// error message
 	int err;
-
+	// string containing the port "ttyTHS2"
 	char* port;
-
+	// callback function for joystick
     void callback(const sensor_msgs::Joy::ConstPtr& Joy);
-
+	// tju_car node
     ros::NodeHandle n;
-
+	// subscriber for receiving joystick information
     ros::Subscriber sub;
-
+	// max linear velocity and max angular velocity
     double MAX_LINEAR_VEL, MAX_ANGULAR_VEL;
 
-    int axis_ang,axis_lin;
+    int axisAngular,axisLinear, brakeButton, startButton;
+
+	int brakeButtonValue, startButtonValue;
+
+	bool isShutdown;
 };
 
--- tju_car/src/tju_car.cpp
+++ tju_car/src/tju_car.cpp
@@ -3,20 +3,29 @@
 
 TjuCar::TjuCar()
 {
+	isShutdown = false;
+
     // Receive max linear vel and max angular vel
 	n.param<double>("max_linear_vel", MAX_LINEAR_VEL, 40);
 	n.param<double>("max_angular_vel", MAX_ANGULAR_VEL, 100);
 
 	// Receive serial port name from launch file
-	std::string serial_port;
-	n.param<std::string>("serial_port", serial_port, "/dev/ttyTHS2");
-	port = new char[serial_port.length() + 1];
-	strcpy(port, serial_port.c_str());
+	std::string serialPort;
+	n.param<std::string>("serial_port", serialPort, "/dev/ttyTHS2");
+	port = new char[serialPort.length() + 1];
+	strcpy(port, serialPort.c_str());
 
     // Receive axis_linear and axis_angular 
     // to get the vel of the specified axes
-    n.param<int>("axis_linear", axis_lin, 1);
-    n.param<int>("axis_angular", axis_ang, 0);
+    n.param<int>("axis_linear", axisLinear, 1);
+    n.param<int>("axis_angular", axisAngular, 0);
+
+	// Receive brake button
+	n.param<int>("brake_button", brakeButton, 7);
+	
+	// Receive start button
+	n.param<int>("start_button", startButton, 6);
+	
     sub = n.subscribe<sensor_msgs::Joy>("joy", 10, &TjuCar::callback, this);
 	
     fd = UART0_Open(fd, port);
@@ -30,53 +39,68 @@ void TjuCar::callback(const sensor_msgs::Joy::ConstPtr& Joy)
 {
     char send_buf[10]={0xff,0xfe,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
 
-    geometry_msgs::Twist v;
-    v.linear.x =Joy->axes[axis_lin]*MAX_LINEAR_VEL;
-    v.angular.z =Joy->axes[axis_ang]*MAX_ANGULAR_VEL;
+	brakeButtonValue = Joy->buttons[brakeButton];
+	if(brakeButtonValue && !isShutdown){
+		isShutdown = true;
+	}
 
-    // convert to 4 wheel velocity
-    double v_a, v_c;
-    v_a = v.linear.x - v.angular.z * 0.179f;
-    v_c = v.linear.x + v.angular.z * 0.179f;
+	startButtonValue = Joy->buttons[startButton];
+	if(startButtonValue && isShutdown){
+		isShutdown = false;	
+	}
 
-    int tmp;
-    char ch_a, ch_c;
-    char flag = 0x00;
-    // a,b wheels
-    if(v_a >= 0){
-            tmp = (int)(v_a + 0.5);
-            ch_a = (char)tmp;
-    }
-    else{
-            tmp = (int)-(v_a - 0.5);
-            ch_a = (char)tmp;
+	geometry_msgs::Twist v;
+	if(!isShutdown){
+		v.linear.x =Joy->axes[axisLinear]*MAX_LINEAR_VEL;
+		v.angular.z =Joy->axes[axisAngular]*MAX_ANGULAR_VEL;
+	}else{
+		v.linear.x = 0;
+		v.angular.z = 0;	
+	}
 
-            flag = flag|0x0c;
-    }
+	// convert to 4 wheel velocity
+	double v_a, v_c;
+	v_a = v.linear.x - v.angular.z * 0.179f;
+	v_c = v.linear.x + v.angular.z * 0.179f;
 
-    // c,d wheels
-    if(v_c >= 0){
-            tmp = (int)(v_c + 0.5);
-            ch_c = (char)tmp;
-    }
-    else{
-            tmp = (int)-(v_c - 0.5);
-            ch_c = (char)tmp;
+	int tmp;
+	char ch_a, ch_c;
+	char flag = 0x00;
+	// a,b wheels
+	if(v_a >= 0){
+	        tmp = (int)(v_a + 0.5);
+	        ch_a = (char)tmp;
+	}
+	else{
+	        tmp = (int)-(v_a - 0.5);
+	        ch_a = (char)tmp;
 
-            flag = flag|0x03;
-    }
+	        flag = flag|0x0c;
+	}
 
-    send_buf[3] = ch_a;
-    send_buf[4] = ch_a;
-    send_buf[5] = ch_c;
-    send_buf[6] = ch_c;
-    send_buf[9] = flag;
-    int len = UART0_Send(fd, send_buf, 10);
-    if (len > 0)
-        printf("send data successful\n");
-    else
-        printf("send data failed!\n");
+	// c,d wheels
+	if(v_c >= 0){
+	        tmp = (int)(v_c + 0.5);
+	        ch_c = (char)tmp;
+	}
+	else{
+	        tmp = (int)-(v_c - 0.5);
+	        ch_c = (char)tmp;
 
-    ROS_INFO("linear:%.3lf angular:%.3lf", v.linear.x, v.angular.z);
-    ROS_INFO("a=b= %x, c=d= %x", ch_a, ch_c);
+	        flag = flag|0x03;
+	}
+
+	send_buf[3] = ch_a;
+	send_buf[4] = ch_a;
+	send_buf[5] = ch_c;
+	send_buf[6] = ch_c;
+	send_buf[9] = flag;
+	int len = UART0_Send(fd, send_buf, 10);
+	if (len > 0)
+	    printf("send data successful\n");
+	else
+	    printf("send data failed!\n");
+
+	ROS_INFO("linear:%.3lf angular:%.3lf", v.linear.x, v.angular.z);
+	ROS_INFO("buttons[7]=%d", brakeButtonValue);
 }
