572d35b73873100182148278074e924f6163b03e
tevenfeng <fengdingwen@outlook.com>
2018-10-18 13:21:00 +0800

    rebuilt with custom installation of opencv3.4 & add python3 node

---
+++ tju_car/nodes/car_cnn_node.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.5
+# coding:utf8
+
+import rospy
+import tensorflow as tf
+import numpy as np
+import cv2 as cv
+
+
+class car_cnn_node(object):
+	def __init__(self):
+		print("Car CNN node init.\n")
+		rospy.init_node('car_cnn_node', anonymous=True)
+		rospy.spin()
+
+
+if __name__ == '__main__':
+	try:
+		car_cnn_node()
+	except rospy.ROSInterruptException:
+		print("Error!\n")
+		pass
