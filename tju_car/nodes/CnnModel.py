9030e315b971bc9bd645d9d9ff7d5d8cb45be1b1
tevenfeng <fengdingwen@outlook.com>
2018-10-26 10:34:28 +0800

    first runnable draft of CNN

---
+++ tju_car/nodes/CnnModel.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python2
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+from TensorflowUtil import weight_variable, bias_variable
+from TensorflowUtil import conv_layer, fc_layer, flattened
+
+
+class CnnModel(object):
+    '''Car CNN model'''
+
+    def __init__(self):
+        self.x = tf.placeholder(tf.float32, [None, 115, 200, 3])
+        self.y_ = tf.placeholder(tf.float32, [None, 1])
+
+        (self.h_conv1, _) = conv_layer(self.x, kernel_shape=(5, 5), stride=2, num_of_kernels=24, use_bias=True)
+        (self.h_conv2, _) = conv_layer(self.h_conv1, kernel_shape=(5, 5), stride=2, num_of_kernels=36, use_bias=True)
+        (self.h_conv3, _) = conv_layer(self.h_conv2, kernel_shape=(5, 5), stride=2, num_of_kernels=48, use_bias=True)
+        (self.h_conv4, _) = conv_layer(self.h_conv3, kernel_shape=(3, 3), stride=1, num_of_kernels=64, use_bias=True)
+        (self.h_conv5, _) = conv_layer(self.h_conv4, kernel_shape=(3, 3), stride=1, num_of_kernels=64, use_bias=True)
+
+        self.h_conv5_flat = flattened(self.h_conv5)
+
+        (self.h_fc1_drop, _, _, self.keep_prob_fc1) = fc_layer(x=self.h_conv5_flat, num_of_neurons=512,
+                                                               activation=tf.nn.relu, use_bias=True, dropout=True)
+        (self.h_fc2_drop, _, _, self.keep_prob_fc2) = fc_layer(self.h_fc1_drop, 100, tf.nn.relu, True, True)
+        (self.h_fc3_drop, _, _, self.keep_prob_fc3) = fc_layer(self.h_fc2_drop, 50, tf.nn.relu, True, True)
+        (self.h_fc4_drop, _, _, self.keep_prob_fc4) = fc_layer(self.h_fc3_drop, 10, tf.nn.relu, True, True)
+        W_fc5 = weight_variable([10, 1])
+        b_fc5 = bias_variable([1])
+
+        self.y_out = tf.matmul(self.h_fc4_drop, W_fc5) + b_fc5
+        self.loss = tf.reduce_mean(tf.abs(tf.subtract(self.y_, self.y_out)))
--- tju_car/nodes/car_cnn_node.py
+++ tju_car/nodes/car_cnn_node.py
@@ -1,23 +1,84 @@
-#!/usr/bin/python3
-# coding:utf8
+#!/usr/bin/python2
 
-import rospy
-import tensorflow as tf
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import numpy as np
-import cv2 as cv
-import cv_bridge
+import tensorflow as tf
+from CnnModel import CnnModel
+
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
+from subprocess import call
+from sensor_msgs.msg import Joy
+import rospy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+"""
+This node restores a saved TF model that was trained on a host computer
+Note:
+1. Toggle the publishing of the twist msg with right bumper on the xbox 360 controller
+2. Steering output from the trained model is mapped from 0.0 to 1.0 where 0.0 is left, 1.0 is right and 0.5 is center
+"""
 
 
-class car_cnn_node(object):
-	def __init__(self):
-		print("Car CNN node init.\n")
-		rospy.init_node('car_cnn_node', anonymous=True)
-		rospy.spin()
+class runCNN(object):
+
+    def __init__(self):
+        # ----------------------------
+        self.sess = tf.InteractiveSession()
+        self.model = CnnModel()
+        saver = tf.train.Saver()
+        saver.restore(self.sess, "/home/nvidia/AutonomousTju/data/model/model.ckpt")
+        # ----------------------------
+        self.startNavigationButton = rospy.get_param('start_navigation_button', 9)
+        self.stopNavigationButton = rospy.get_param('stop_navigation_button', 8)
+        self.bridge = CvBridge()
+        self.netEnable = False
+        rospy.Subscriber("/usb_cam/image_raw", Image, self.image_callback)
+        rospy.Subscriber("/joy", Joy, self.joy_callback)
+        self.joy_pub = rospy.Publisher("/navigation", Joy, queue_size=1)
+        rospy.init_node('car_cnn_node', anonymous=True)
+        rospy.spin()
+
+    def image_callback(self, pic):
+        if self.netEnable:
+            joy = Joy()
+            cv2image = self.bridge.imgmsg_to_cv2(pic)
+            cv2image = cv2.resize(cv2image, (200, 150), interpolation=cv2.INTER_CUBIC)
+            cv2image = cv2image[35:, :, :]
+            normed_img = cv2image.astype(dtype=np.float32) / 255.0
+            # normed_img = np.reshape(normed_img, (115, 200, 1))
+            steer = self.model.y_out.eval(session=self.sess, feed_dict={self.model.x: [normed_img],
+                                                                        self.model.keep_prob_fc1: 1.0,
+                                                                        self.model.keep_prob_fc2: 1.0,
+                                                                        self.model.keep_prob_fc3: 1.0,
+                                                                        self.model.keep_prob_fc4: 1.0})
+            joy.axes.append(steer[0][0])
+            joy.axes.append(1.0)			
+			#joy.axes[1] = 1.0
+            #joy.axes[0] = steer[0][0]
+            print("steering angle = %s\n" % str(joy.axes[0]))
+            self.joy_pub.publish(joy)
+
+    def joy_callback(self, joy):
+        start_navigation_button_value = joy.buttons[self.startNavigationButton]
+        if start_navigation_button_value and (not self.netEnable):
+            self.netEnable = True
+            print('Neural Network Enabled!\n')
+
+        stop_navigation_button_value = joy.buttons[self.stopNavigationButton]
+        if stop_navigation_button_value and self.netEnable:
+            self.netEnable = False
+            print('Neural Network Disabled!\n')
 
 
 if __name__ == '__main__':
-	try:
-		car_cnn_node()
-	except rospy.ROSInterruptException:
-		print("Error!\n")
-		pass
+    try:
+        runCNN()
+    except rospy.ROSInterruptException:
+        pass
