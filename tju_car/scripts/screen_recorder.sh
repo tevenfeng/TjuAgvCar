3307137a0215c65ccd1d45bdc7abe49ce7a381b5
tevenfeng <fengdingwen@outlook.com>
2019-04-06 14:45:24 +0800

    add script to record screen for demo

---
+++ tju_car/scripts/screen_recorder.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gst-launch-1.0 -v ximagesrc use-damage=0 ! nvvidconv ! 'video/x-raw(memory:NVMM),alignment=(string)au,format=(string)I420,framerate=(fraction)25/1,pixel-aspect-ratio=(fraction)1/1' ! omxh264enc !  'video/x-h264,stream-format=(string)byte-stream' ! h264parse ! matroskamux ! filesink location=/home/nvidia/AutonomousTju/data/screen.mkv
